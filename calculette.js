bcb40642eec539d9b244f44cd916511091f5f775
farouk <f.bejaoui1@gmail.com>
2017-12-11 17:08:58 +0100

    Version cool

--- calculette.js
+++ calculette.js
@@ -1,5 +1,57 @@
+var numb = document.querySelectorAll('.numb');
+var input = document.querySelectorAll('input')[0];
+for (i=0; i<numb.length; i++){
+numb[i].addEventListener('click', function ( event ){
+        input.value = input.value + event.target.textContent;
+    }, true);
+}
+var ope = document.querySelectorAll('.ope');
+for (i=0; i<ope.length; i++){
+ope[i].addEventListener('click', function ( event ){
+        input.value = input.value + event.target.textContent;
+    }, true);
+}
+var egal = document.querySelectorAll('input')[1];
+egal.addEventListener('click', function ( event ){
+    input.value = eval(input.value);
+}, true);
+var cancel = document.querySelector('.reset');
+cancel.addEventListener('click', function (event) {
+    input.value = "";
+}, true);   
 
-function calcul(){
+
+
+
+
+/*
+numb.addEventListener('click', function (){
+    var input = document.querySelector('input');
+    var numb = document.querySelectorAll('.numb');
+        for (i=0; i<numb.length ; i++)
+        {
+        numb = document.querySelectorAll('.numb')[i];
+        input.value = numb.textContent;
+        }
+    });
+*/
+
+
+
+
+
+
+
+/*
+for (i=0; i<numb.length ; i++){
+numb = document.querySelectorAll('.numb')[i];
+console.log(numb);
+numb.addEventListener('click', print);
+}
+
+
+*/
+/*function calcul(){
     var a = document.querySelectorAll('input')[0].value;
     var b = document.querySelectorAll('input')[1].value;
     var operator = document.querySelector('#select').options[select.selectedIndex].value;
@@ -24,3 +76,4 @@ function calcul(){
 }
     var submit = document.querySelectorAll('input')[2];
     submit.addEventListener('click', calcul);
+*/
